2ab4d98668aefa4ca23ca67d9d3bab9668f5bf30
kratley <kratley31@gmail.com>
2022-12-13 20:51:19 -0600

    Added City variable

--- src/app.js
+++ src/app.js
@@ -37,7 +37,8 @@ function displayTemperature(response) {
 }
 
 let apiKey = "2513f3c728b1b5ff4f4347e1a6af22b8";
-let apiUrl = `https:api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=imperial`;
+let city = "Paris";
+let apiUrl = `https:api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
 
 // "https://api.shecodes.io/weather/v1/current?query={query}&key={key}";
 
