1ce7acb7be4a0491d56ca13fbf9c13aca301a058
kratley <kratley31@gmail.com>
2022-12-13 18:37:59 -0600

    Troubleshoot and start API setup

--- src/app.js
+++ src/app.js
@@ -1,1 +1,6 @@
+let apiKey = "2513f3c728b1b5ff4f4347e1a6af22b8";
+let apiUrl = `https:api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
 
+// "https://api.shecodes.io/weather/v1/current?query={query}&key={key}";
+
+console.log(apiUrl);
