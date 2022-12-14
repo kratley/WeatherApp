8d2f012e8468002472af5866591d334e2f190734
kratley <kratley31@gmail.com>
2022-12-13 20:27:58 -0600

    Showing current temp and description

--- src/app.js
+++ src/app.js
@@ -1,6 +1,22 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
 let apiKey = "2513f3c728b1b5ff4f4347e1a6af22b8";
-let apiUrl = `https:api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+let apiUrl = `https:api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=imperial`;
 
 // "https://api.shecodes.io/weather/v1/current?query={query}&key={key}";
 
 console.log(apiUrl);
+
+axios.get(apiUrl).then(displayTemperature);
