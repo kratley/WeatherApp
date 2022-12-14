801c3f765c17a041ab79c875e43e71b7cbb0b19b
kratley <kratley31@gmail.com>
2022-12-13 21:21:55 -0600

    Added search engine

--- src/app.js
+++ src/app.js
@@ -42,14 +42,20 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "2513f3c728b1b5ff4f4347e1a6af22b8";
-let city = "New York";
-let apiUrl = `https:api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+function search(city) {
+  let apiKey = "2513f3c728b1b5ff4f4347e1a6af22b8";
+  let apiUrl = `https:api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-// "https://api.shecodes.io/weather/v1/current?query={query}&key={key}";
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+  console.log(cityInputElement.value);
+}
 
-console.log(apiUrl);
+search("New York");
 
-new Date(1670984204 * 1000);
-
-axios.get(apiUrl).then(displayTemperature);
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
