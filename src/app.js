103e038dc2a6c582a52736a229841f30cbe5177c
kratley <kratley31@gmail.com>
2022-12-18 14:49:12 -0600

    Display forecast data using API

--- src/app.js
+++ src/app.js
@@ -21,28 +21,47 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+
 function displayForecast(response) {
+  let forecast = response.data.daily;
+
   let forcastElement = document.querySelector("#forecast");
 
   let forecastHTML = `   <div class="row"> `;
-  let days = ["Thu", "Fri", "Sat", "Sun"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
               <div class="col-2">
-                <div class="weather-forcast-date">${day}</div>
+                <div class="weather-forcast-date">${formatDay(
+                  forecastDay.dt
+                )}</div>
                 <img
-                  src="http://openweathermap.org/img/wn/02d@2x.png"
+                  src="http://openweathermap.org/img/wn/${
+                    forecastDay.weather[0].icon
+                  }@2x.png"
                   alt="description"
                   width="42"
                 />
                 <div class="weather-forcast-tempatures">
-                  <span class="weather-forcast-tempaturers-max"> 18° </span>
-                  <span class="weather-forcast-tempatures-min"> 12° </span>
+                  <span class="weather-forcast-tempaturers-max">${Math.round(
+                    forecastDay.temp.max
+                  )}° </span>
+                  <span class="weather-forcast-tempatures-min">${Math.round(
+                    forecastDay.temp.min
+                  )}° </span>
                 </div>
               </div>        
   `;
+    }
   });
 
   forecastHTML = forecastHTML + ` </div>`;
